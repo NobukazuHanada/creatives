9de7a85e0c5aa56577e4d1d193007b1339af377b
NobukazuHanada <tone.nobukazu@gmail.com>
2015-08-09 10:58:38 +0900

    make flower

---
+++ purescript/second/src/Main.js
@@ -0,0 +1,13 @@
+/* global exports, Main */
+"use strict;"
+
+// module Main
+
+exports.windowWidth = function(){
+    return window.document.documentElement.clientWidth;    
+}
+
+
+exports.windowHeight = function(){
+    return window.document.documentElement.clientHeight; 
+}
--- purescript/second/src/Main.purs
+++ purescript/second/src/Main.purs
@@ -11,51 +11,66 @@ import Control.Monad.Eff.Random
 import Control.Apply
 import Graphics.Canvas
 import Data.Maybe
+import DOM
+
+foreign import windowWidth :: forall eff. Eff ( dom :: DOM | eff ) Number
+foreign import windowHeight :: forall eff. Eff ( dom :: DOM | eff ) Number
 
 main = do
   Just element <- getCanvasElementById "canvas"
-  setCanvasWidth 1000.0 element
-  setCanvasHeight 600.0 element
+  width <- windowWidth
+  height <- windowHeight
+  setCanvasWidth width element
+  setCanvasHeight height element
   context <- getContext2D element
-  beginPath context
-  translate { translateX : 500.0, translateY : 300.0 } context
-  draw context
-  setLineWidth 1.0 context
-  setStrokeStyle "#BBBAB9" context
-  stroke context
-
+  forE 0.0 50.0 (\i -> do
+       x <- map (* width) random
+       y <- map (* height) random
+       r <- map (\x -> x * 70.0 + 50.0) random
+       color <- map (\x -> "hsla(" ++ show (x * 360.0) ++ ",100%,50%, 0.4)") random
+       draw context x y r color
+       return unit
+  )
   
-draw context =
-  forE 1.0 10000.0 (\i ->
+draw context x y radius color = do
+  beginPath context
+  translate { translateX : x, translateY : y } context
+  setGlobalAlpha context 0.0
+  setLineWidth 2.5 context
+  setFillStyle color context
+  forE 0.0 10000.0 (\i ->
        let
         -- previus
         preAngle = M.pi * 2.0 * (i - 1.0) / 100.0
-        preR = func (preAngle * 10.0) + 200.0
+        preR = (func (preAngle * 10.0) radius) + radius
         prePos = V.scale preR $ V.vec2 (M.sin preAngle) (M.cos preAngle)
         -- current
         angle = M.pi * 2.0 * i / 100.0
-        r = func (angle * 10.0) + 200.0  
+        r = (func (angle * 10.0) radius) + radius
         pos = V.scale r $ V.vec2 (M.sin angle) (M.cos angle)
        in
         do
           line context prePos pos
           return unit
   )
+  closePath context
+  fill context
+  translate { translateX : (0.0 - x), translateY : (0.0 - y) } context
+  
 
-func angle = 
+func angle radius =      
      let   
-       a = 10.0
+       a = radius / 20.0
      in
         a * (10.0 * M.pow (M.sin $ angle / 2.0 + 1.0) 3.0) + (20.0 * (M.sin $ angle + 3.0))
 
 
 line context origin to =
   let
-    originX = V.get2X origin
-    originY = V.get2Y origin
-    toX = V.get2X to    
-    toY = V.get2Y to
-  in do
-     moveTo context originX originY
+    originX = V.get2X origin + 0.5
+    originY = V.get2Y origin + 0.5
+    toX = V.get2X to + 0.5
+    toY = V.get2Y to + 0.5
+  in 
      lineTo context toX toY
 
