d57032bdb9db31c6d8a83eccf8c89745dc31caf3
NobukazuHanada <tone.nobukazu@gmail.com>
2015-08-09 17:44:42 +0900

    flower rotate

--- purescript/second/src/Main.purs
+++ purescript/second/src/Main.purs
@@ -9,45 +9,57 @@ import Control.Monad.Eff.Console
 import Control.Monad.ST
 import Control.Monad.Eff.Random
 import Control.Apply
+import Data.Array.ST
 import Graphics.Canvas
 import Data.Maybe
 import DOM
+import Signal
+import Signal.Time
 
 foreign import windowWidth :: forall eff. Eff ( dom :: DOM | eff ) Number
 foreign import windowHeight :: forall eff. Eff ( dom :: DOM | eff ) Number
+
 
 main = do
   Just element <- getCanvasElementById "canvas"
   width <- windowWidth
   height <- windowHeight
   setCanvasWidth width element
-  setCanvasHeight height element
+  setCanvasHeight height element  
   context <- getContext2D element
-  forE 0.0 50.0 (\i -> do
+  initRef <- emptySTArray
+  forE 0.0 50.0 (\_ -> do
        x <- map (* width) random
        y <- map (* height) random
        r <- map (\x -> x * 70.0 + 50.0) random
-       color <- map (\x -> "hsla(" ++ show (x * 360.0) ++ ",100%,50%, 0.4)") random
-       draw context x y r color
-       return unit
-  )
+       c <- map (\x -> "hsla(" ++ show (x * 360.0) ++ ",100%,50%, 0.4)") random
+       pushSTArray initRef $ {x:x, y:y, r:r, c:c}
+       return unit)    
+  runSignal $ loop ~> (\angle -> do
+       clearRect context {x:0.0,y:0.0,w:width,h:height}
+       init <- toAssocArray initRef
+       foreachE init (\{index:_, value:{x:x,y:y,r:r,c:c}} -> do
+                             draw context x y r (angle * (r/100.0)) c
+                             return unit))
+
+loop = foldp (\a b -> b + 0.2) 0.0 (every $ 50.0 * millisecond)
   
-draw context x y radius color = do
+draw context x y radius baseAngle color = do
   beginPath context
   translate { translateX : x, translateY : y } context
   setGlobalAlpha context 0.0
   setLineWidth 2.5 context
   setFillStyle color context
-  forE 0.0 10000.0 (\i ->
+  forE 0.0 samples (\i ->
        let
         -- previus
-        preAngle = M.pi * 2.0 * (i - 1.0) / 100.0
+        preAngle = M.pi * 2.0 * (i - 1.0) / samples
         preR = (func (preAngle * 10.0) radius) + radius
         prePos = V.scale preR $ V.vec2 (M.sin preAngle) (M.cos preAngle)
         -- current
-        angle = M.pi * 2.0 * i / 100.0
+        angle = M.pi * 2.0 * i / samples
         r = (func (angle * 10.0) radius) + radius
-        pos = V.scale r $ V.vec2 (M.sin angle) (M.cos angle)
+        pos = V.scale r $ V.vec2 (M.sin $ angle + baseAngle) (M.cos $ angle + baseAngle)
        in
         do
           line context prePos pos
@@ -56,6 +68,8 @@ draw context x y radius color = do
   closePath context
   fill context
   translate { translateX : (0.0 - x), translateY : (0.0 - y) } context
+  where 
+        samples = 200.0
   
 
 func angle radius =      
