e8f748e33b258d3ab51506e45db024ce8b020d4a
NobukazuHanada <tone.nobukazu@gmail.com>
2015-08-12 02:28:20 +0900

    include parenscript

---
+++ common-lisp/first/first.lisp
@@ -0,0 +1,12 @@
+
+
+(defun main (window)
+  (play window))
+
+(defun play (window)
+  (let* ((*audio-cxt (@ window *audio-context))
+         (audio-context (new (*audio-cxt)))
+         (osc-node (chain audio-context (create-oscillator))))
+    (setf (@ osc-node type) "square")
+    (setf (@ osc-node frequency value) 2000)
+    (chain osc-node (connect (@ audio-context destination)))))
